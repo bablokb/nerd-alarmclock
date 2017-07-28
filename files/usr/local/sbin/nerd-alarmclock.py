3a9b19ac325c06f179c1a6c091dacfe41522b486
Bernhard Bablok <bablokb@gmx.de>
2017-07-28 12:02:50 +0200

    create AlarmController and add to settings

--- files/usr/local/sbin/nerd-alarmclock.py
+++ files/usr/local/sbin/nerd-alarmclock.py
@@ -64,6 +64,7 @@ def init(parser):
 
   settings.leds    = nclock.LedController.LedController(settings)
   settings.display = nclock.DisplayController.DisplayController(settings)
+  settings.alarms  = nclock.AlarmController.AlarmController(settings)
   return settings
 
 # --- start all threads   --------------------------------------------------
